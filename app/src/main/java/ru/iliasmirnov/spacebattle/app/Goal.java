9ff475bc6f21d37f609fbcf6c49ec14c16f72358
ilyacheater <ilyacheater@gmail.com>
2015-03-30 17:01:06 +0300

    changes

--- app/src/main/java/ru/iliasmirnov/spacebattle/app/Goal.java
+++ app/src/main/java/ru/iliasmirnov/spacebattle/app/Goal.java
@@ -1,5 +1,37 @@
 package ru.iliasmirnov.spacebattle.app;
 
 public class Goal {
+    private float startX, startY, endX, endY;
 
+    public float getStartX() {
+        return startX;
+    }
+
+    public void setStartX(float startX) {
+        this.startX = startX;
+    }
+
+    public float getStartY() {
+        return startY;
+    }
+
+    public void setStartY(float startY) {
+        this.startY = startY;
+    }
+
+    public float getEndX() {
+        return endX;
+    }
+
+    public void setEndX(float endX) {
+        this.endX = endX;
+    }
+
+    public float getEndY() {
+        return endY;
+    }
+
+    public void setEndY(float endY) {
+        this.endY = endY;
+    }
 }
